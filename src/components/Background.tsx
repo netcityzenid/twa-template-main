d1d659467776bc0847023b7b90753ec6387f053b
Netcityzen <netcityzen.id@gmail.com>
2024-07-31 21:25:54 +0700

    ee

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Background() {
-  return <div className="h-[200vh] pb-40 fixed top-0 bottom-0 left-0 right-0  bg-[#111888] flex w-full"></div>;
+  return <div className="h-[200vh] pb-40 fixed top-0 bottom-0 left-0 right-0   bg-[#111888] flex w-full"></div>;
 }
